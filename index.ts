f52d310702339cc2803af47c12b0c472ebfa0a6f
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-03 17:50:26 +0100

    feat: basic class structure setup
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

---
+++ index.ts
@@ -0,0 +1,15 @@
+class Flytrap {
+  private projectId: string;
+  private apiEndpoint: string;
+  private apiKey: string;
+
+  constructor(config: {
+    projectId: string;
+    apiEndpoint: string;
+    apiKey: string;
+  }) {
+    this.projectId = config.projectId;
+    this.apiEndpoint = config.apiEndpoint;
+    this.apiKey = config.apiKey;
+  }
+}
