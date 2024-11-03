f38355e950159f3ea61ed5a4473f48ac6ea9c906
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-03 17:58:22 +0100

    feat: implement global unhandled rejection handling
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -16,11 +16,24 @@ class Flytrap {
   // * --- Private Methods --- * //
   private setUpGlobalErrorHandlers(): void {
     process.on('uncaughtException', (e: Error) => this.handleUncaughtException(e));
+    process.on('unhandledRejection', (reason: any, promise: Promise<any>) => 
+      this.handleUnhandledRejection(reason, promise)
+    );
   }
 
   private handleUncaughtException(e: Error): void {
     if (e.error instanceof FlytrapError) return;
     this.logError(e, false);
+    // process.exit(1); // Uncomment if needed
+  }
+
+  private handleUnhandledRejection(reason: any, promise: Promise<any>): void {
+    if (reason instanceof Error) {
+      if (reason instanceof FlytrapError) return;
+      this.#logError(reason, false);
+    } else {
+      this.#logPromiseRejection(reason, false);
+    }
     // process.exit(1); // Uncomment if needed
   }
 }
